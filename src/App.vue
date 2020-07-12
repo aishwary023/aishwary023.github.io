c8248f9876281bdfea74409360775d36a8b00a83
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2020-07-12 16:43:12 +0530

    Added basic structure for the project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
+    <AboutMe />
+    <Projects />
+    <ContactMe />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './components/Home.vue';
+import AboutMe from './components/AboutMe.vue';
+import Projects from './components/Projects';
+import ContactMe from './components/ContactMe';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
+    AboutMe,
+    ContactMe,
+    Projects
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="home"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
