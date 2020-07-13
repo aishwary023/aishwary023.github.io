8344ae94e63d8a76843bc0cd69aa0963de69848f
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2020-07-13 11:37:09 +0530

    Changes typography

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,81 +1,87 @@
 <template>
-  <div class="container" id="about">
-    <span>
-      <h3>
-        About me
-      </h3>
-    </span>
-    <hr />
-    <body>
-      <br />
+<div class="container" id="about">
+  <span>
+    <h3>About Me</h3>
+  </span>
+  <hr />
+  <body>
+    <br />
 
-      <p style="text-align:center">
-        <img
-          style="border-radius:50%"
-          src="../assets/aishwary.png"
-          alt="Aishwary Saxena"
-          height="200px"
-          width="200px"
-        />
-      </p>
+    <p style="text-align:center">
+      <img
+        style="border-radius:50%"
+        src="../assets/aishwary.png"
+        alt="Aishwary Saxena"
+        height="200px"
+        width="200px"
+      />
+    </p>
+    <br />
+    <p align="center" style="font-size:16px">
+      💬 student, self-taught coder
+      <br />🌱 learning Django. REST. VueJS. Steganography. Cryptography
+      <br />🔭 intrested in web-development, info-sec, anything tech
+      <br />🖥 develops webapps. back-end works.
+      <br />🧩 likes painting. cycling. dancing. and obviously coding
       <br />
-      <p align="center" style="font-size:16px">
-        💬 student, self-taught coder<br />
-        🌱 learning Django. REST. VueJS. Steganography. Cryptography<br />
-        🔭 intrested in web-development, info-sec, anything tech <br />
-        🖥 develops webapps. back-end works.<br />
-        🧩 likes painting. cycling. dancing. and obviously coding<br />
-      </p>
-      <p><br /></p>
-      <p align="center">
-        <a
-          class="contact-icon"
-          href="https://www.github.com/aishwary023/"
-          target="_blank"
-          title="GitHub"
-          ><img
-            height="32"
-            src="https://raw.githubusercontent.com/feathericons/feather/master/icons/github.svg"
-        /></a>
-        <a
-          class="contact-icon"
-          href="https://www.linkedin.com/in/aishwary-saxena/"
-          target="_blank"
-          title="LinkedIn"
-          ><img
-            height="32"
-            src="https://raw.githubusercontent.com/feathericons/feather/master/icons/linkedin.svg"
-        /></a>
-        <a
-          class="contact-icon"
-          href="mailto:aishwary.saxena.min19@itbhu.ac.in"
-          target="_blank"
-          title="Gmail"
-          ><img
-            height="32"
-            src="https://raw.githubusercontent.com/feathericons/feather/master/icons/mail.svg"
-        /></a>
-        <a
-          class="contact-icon"
-          href="https://wa.me/918299329212"
-          target="_blank"
-          title="Whatsapp"
-          ><img
-            height="32"
-            src="https://raw.githubusercontent.com/feathericons/feather/master/icons/message-circle.svg"
-        /></a>
-        <a
-          class="contact-icon"
-          href="https://www.instagram.com/ai.shwary/"
-          target="_blank"
-          title="Instagram"
-          ><img
-            height="32"
-            src="https://raw.githubusercontent.com/feathericons/feather/master/icons/instagram.svg"
-        /></a>
-      </p>
-    </body>
-  </div>
+    </p>
+    <p>
+      <br />
+    </p>
+    <p align="center">
+      <a
+        class="contact-icon"
+        href="https://www.github.com/aishwary023/"
+        target="_blank"
+        title="GitHub"
+      >
+        <img
+          height="32"
+          src="https://raw.githubusercontent.com/feathericons/feather/master/icons/github.svg"
+        />
+      </a>
+      <a
+        class="contact-icon"
+        href="https://www.linkedin.com/in/aishwary-saxena/"
+        target="_blank"
+        title="LinkedIn"
+      >
+        <img
+          height="32"
+          src="https://raw.githubusercontent.com/feathericons/feather/master/icons/linkedin.svg"
+        />
+      </a>
+      <a
+        class="contact-icon"
+        href="mailto:aishwary.saxena.min19@itbhu.ac.in"
+        target="_blank"
+        title="Gmail"
+      >
+        <img
+          height="32"
+          src="https://raw.githubusercontent.com/feathericons/feather/master/icons/mail.svg"
+        />
+      </a>
+      <a class="contact-icon" href="https://wa.me/918299329212" target="_blank" title="Whatsapp">
+        <img
+          height="32"
+          src="https://raw.githubusercontent.com/feathericons/feather/master/icons/message-circle.svg"
+        />
+      </a>
+      <a
+        class="contact-icon"
+        href="https://www.instagram.com/ai.shwary/"
+        target="_blank"
+        title="Instagram"
+      >
+        <img
+          height="32"
+          src="https://raw.githubusercontent.com/feathericons/feather/master/icons/instagram.svg"
+        />
+      </a>
+    </p>
+  </body>
+</div>
 </template>
 
 <script>
@@ -91,5 +97,11 @@ export default {};
 }
 body {
   color: #000000;
+}
+h3 {
+  font-family: 'Source Code Pro', monospace;
+  font-size: 32px;
+  color: #494949;
+  font-weight: 200;
 }
 </style>
