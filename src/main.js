5d64acfdf71ab41a1e2066f84089586a4e1714ed
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2020-07-14 07:22:48 +0530

    Added vue gtag

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueAnalytics from 'vue-analytics';
-Vue.use(VueAnalytics, {
-  id: 'UA-172619189-1'
-});
 
 Vue.config.productionTip = false;
 
 var VueScrollTo = require('vue-scrollto');
 
 Vue.use(VueScrollTo);
+import VueGtag from 'vue-gtag';
 
+Vue.use(VueGtag, {
+  config: { id: 'UA-172619189-1' }
+});
 new Vue({
   render: (h) => h(App)
 }).$mount('#app');
